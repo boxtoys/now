c2b671628555adb6e9e42e1061df96dc928a8edd
2046 <2046@ufo.codes>
2023-09-23 23:27:03 +0800

    chore: optimization code

--- shell/node.mjs
+++ shell/node.mjs
@@ -3,33 +3,78 @@
 const HOST = 'https://now.box.toys'
 const BASE_URL = `${HOST}/templates/node`
 
-await mkdir('.husky')
-await cp('.husky/pre-commit')
-await cp('.husky/commit-msg')
+getProjectName()
+  .then(checkDirectoryExists)
+  .then(generateFiles)
+  .then(printSuccessMessage)
+  .catch(reason => echo(reason))
 
-await mkdir('src')
-await cp('src/index.ts')
+async function getProjectName() {
+  const result = await question('Project name: ')
 
-await cp('.commitlintrc')
-await cp('.cz-config.js')
-await cp('.czrc')
-await cp('.eslintrc.js')
-await cp('.gitignore')
-await cp('.lintstagedrc')
-await cp('.prettierrc')
-await cp('package.json')
-await cp('tsconfig.json')
+  if (!result.trim()) {
+    throw new Error('Project name is required.')
+  } else {
+    return result
+  }
+}
 
-await $`git init`
+async function checkDirectoryExists(projectName) {
+  if (await exists(projectName)) {
+    throw new Error(`Directory ${projectName} already exists.`)
+  } else {
+    return projectName
+  }
+}
 
-echo``
-echo`Scaffolding project in ${await $`pwd`}...`
-echo``
-echo`Done. Now run:`
-echo``
-echo`  npm i`
-echo`  npm run dev`
-echo``
+async function generateFiles(projectName) {
+  await mkdir(projectName)
+
+  await within(async () => {
+    await cd(projectName)
+
+    await spinner('Generated files...', async () => {
+      await mkdir('.husky')
+      await cp('.husky/pre-commit')
+      await cp('.husky/commit-msg')
+
+      await mkdir('src')
+      await cp('src/index.ts')
+
+      await cp('.commitlintrc')
+      await cp('.cz-config.js')
+      await cp('.czrc')
+      await cp('.eslintrc.js')
+      await cp('.gitignore')
+      await cp('.lintstagedrc')
+      await cp('.prettierrc')
+      await cp('package.json')
+      await cp('tsconfig.json')
+
+      await $`git init`
+    })
+  })
+
+  return projectName
+}
+
+async function printSuccessMessage(projectName) {
+  echo``
+  echo`Scaffolding project in ${await $`pwd`}${path.sep}${projectName}...`
+  echo``
+  echo`Done. Now run:`
+  echo``
+  echo`  cd ${projectName}`
+  echo`  npm i`
+  echo`  npm run dev`
+  echo``
+}
+
+async function exists(dirName) {
+  const pwd = await $`pwd`
+  
+  return fs.existsSync(path.join(pwd.stdout.replace('\n', ''), dirName))
+}
 
 async function mkdir(dirName) {
   await $`mkdir ${dirName}`.nothrow()
--- shell/node.part.mjs
+++ shell/node.part.mjs
@@ -3,41 +3,86 @@
 const HOST = 'https://now.box.toys'
 const BASE_URL = `${HOST}/templates/node.part`
 
-await mkdir('.husky')
-await cp('.husky/pre-commit')
-await cp('.husky/commit-msg')
+getProjectName()
+  .then(checkDirectoryExists)
+  .then(generateFiles)
+  .then(printSuccessMessage)
+  .catch(reason => echo(reason))
 
-await mkdir('src')
-await cp('src/index.ts')
+async function getProjectName() {
+  const result = await question('Project name: ')
 
-await mkdir('test')
-await cp('test/hello.test.ts')
+  if (!result.trim()) {
+    throw new Error('Project name is required.')
+  } else {
+    return result
+  }
+}
 
-await cp('.browserslistrc')
-await cp('.commitlintrc')
-await cp('.cz-config.js')
-await cp('.czrc')
-await cp('.eslintrc.js')
-await cp('.gitignore')
-await cp('.lintstagedrc')
-await cp('.npmignore')
-await cp('.prettierrc')
-await cp('babel.config.json')
-await cp('jest.config.json')
-await cp('package.json')
-await cp('rollup.config.js')
-await cp('tsconfig.json')
+async function checkDirectoryExists(projectName) {
+  if (await exists(projectName)) {
+    throw new Error(`Directory ${projectName} already exists.`)
+  } else {
+    return projectName
+  }
+}
 
-await $`git init`
+async function generateFiles(projectName) {
+  await mkdir(projectName)
 
-echo``
-echo`Scaffolding project in ${await $`pwd`}...`
-echo``
-echo`Done. Now run:`
-echo``
-echo`  npm i`
-echo`  npm run dev`
-echo``
+  await within(async () => {
+    await cd(projectName)
+
+    await spinner('Generated files...', async () => {
+      await mkdir('.husky')
+      await cp('.husky/pre-commit')
+      await cp('.husky/commit-msg')
+
+      await mkdir('src')
+      await cp('src/index.ts')
+
+      await mkdir('test')
+      await cp('test/hello.test.ts')
+
+      await cp('.browserslistrc')
+      await cp('.commitlintrc')
+      await cp('.cz-config.js')
+      await cp('.czrc')
+      await cp('.eslintrc.js')
+      await cp('.gitignore')
+      await cp('.lintstagedrc')
+      await cp('.npmignore')
+      await cp('.prettierrc')
+      await cp('babel.config.json')
+      await cp('jest.config.json')
+      await cp('package.json')
+      await cp('rollup.config.js')
+      await cp('tsconfig.json')
+
+      await $`git init`
+    })
+  })
+
+  return projectName
+}
+
+async function printSuccessMessage(projectName) {
+  echo``
+  echo`Scaffolding project in ${await $`pwd`}${path.sep}${projectName}...`
+  echo``
+  echo`Done. Now run:`
+  echo``
+  echo`  cd ${projectName}`
+  echo`  npm i`
+  echo`  npm run dev`
+  echo``
+}
+
+async function exists(dirName) {
+  const pwd = await $`pwd`
+  
+  return fs.existsSync(path.join(pwd.stdout.replace('\n', ''), dirName))
+}
 
 async function mkdir(dirName) {
   await $`mkdir ${dirName}`.nothrow()
