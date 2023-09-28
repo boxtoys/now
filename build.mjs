9fb19427753e0cd11a60eaa4c880d2c842cbf065
2046 <2046@ufo.codes>
2023-09-28 17:42:59 +0800

    chore: add build script

---
+++ build.mjs
@@ -0,0 +1,40 @@
+#!/usr/bin/env zx
+
+import * as dotenv from 'dotenv'
+
+const exclude = fs.readFileSync('.buildignore').toString().split('\n').filter((line) => line !== '')
+const env = dotenv.parse(await fs.readFile(`.env`, 'utf8'))
+const files = getAllFilePaths(__dirname)
+
+if (argv.env === 'development') {
+  for (const file of files) {
+    const content = await fs.readFile(file, 'utf8')
+    const result = content.replace(new RegExp(env.PRODUCTION_HOST, 'g'), env.DEVELOPMENT_HOST)
+    await fs.writeFile(file, result, 'utf8')
+  }
+}
+
+if (argv.env === 'production') {
+  for (const file of files) {
+    const content = await fs.readFile(file, 'utf8')
+    const result = content.replace(new RegExp(env.DEVELOPMENT_HOST, 'g'), env.PRODUCTION_HOST)
+    await fs.writeFile(file, result, 'utf8')
+  }
+}
+
+function getAllFilePaths(dirPath) {
+  const filePaths = []
+  const files = fs.readdirSync(dirPath).filter((file) => !exclude.includes(file))
+
+  for (const file of files) {
+    const filePath = path.join(dirPath, file)
+    
+    if (fs.lstatSync(filePath).isDirectory()) {
+      filePaths.push(...getAllFilePaths(filePath))
+    } else {
+      filePaths.push(filePath)
+    }
+  }
+
+  return filePaths
+}
